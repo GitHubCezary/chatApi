4753a4e67ac77b2d85a6abec86836d6e3c124b50
GitHubCezary <cezary.konowrocki@wp.pl>
2024-01-22 22:43:06 +0100

    add chat user model db config and repo added.

---
+++ src/main/java/pl/chatApi/Models/ChatUser.java
@@ -0,0 +1,46 @@
+package pl.chatApi.Models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class ChatUser {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+
+
+    public ChatUser(String username, String password) {
+        this.username = username;
+        this.password = password;
+
+    }
+
+    public ChatUser() {
+
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+}
